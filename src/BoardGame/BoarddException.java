60b6788619335bd413b3ab9e3a21b6748fb8a20a
marcosr1 <marcosrichelly43@gmail.com>
2025-04-09 16:19:58 -0300

    BoardException and defensive programming

---
+++ src/BoardGame/BoarddException.java
@@ -0,0 +1,11 @@
+package BoardGame;
+
+public class BoarddException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public BoarddException(String msg) {
+		super(msg);
+	}
+	
+	
+}
