33f863730b1f34ae147251e78019a0b56d3e56e9
marcosr1 <marcosrichelly43@gmail.com>
2025-04-09 17:25:20 -0300

    Moving pieces

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,14 +1,32 @@
 package Application;
 
+import java.util.Scanner;
+
 import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		
-		UI.printBoard(chessMatch.getPieces());
+		while (true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			
+			System.out.print("Source: ");
+			ChessPosition soucer = UI.readChessPosition(sc);
+			System.out.println();
+			
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(soucer, target);
+		}
+		
 		
 	}	
 }
--- src/Chess/ChessException.java
+++ src/Chess/ChessException.java
@@ -1,6 +1,8 @@
 package Chess;
 
-public class ChessException extends RuntimeException {
+import BoardGame.BoarddException;
+
+public class ChessException extends BoarddException {
 	private static final long serialVersionUID = 1L;
 
 	public ChessException(String msg) {
