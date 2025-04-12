05b50978fef681ad224b35a7b9ea662e209d5bec
marcosr1 <marcosrichelly43@gmail.com>
2025-04-12 11:11:47 -0300

    Queen

--- src/Chess/pieces/Queen.java
+++ src/Chess/pieces/Queen.java
@@ -5,67 +5,62 @@ import BoardGame.Position;
 import Chess.ChessPiece;
 import Chess.Color;
 
-public class Knight extends ChessPiece {
-
-	public Knight(Board board, Color color) {
-		super(board, color);
-	}
+public class Queen extends ChessPiece {
 	
+	public Queen(Board board, Color color) {
+		super(board, color);
+
+	}
+
 	@Override
 	public String toString() {
-		return "N";
+		return "Q";
 	}
 	
-	private boolean canMove(Position position) {
-		ChessPiece p = (ChessPiece)getBoard() .piece(position);
-		return p == null || p.getColor() != getColor();
-	}
-
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		
 		Position p = new Position(0, 0);
 		
-		p.setValues(position.getRow()- 1, position.getColumn() - 2);
-		if (getBoard().positionExists(p) && canMove(p)) {
+		//above
+		p.setValues(position.getRow() - 1, position.getColumn() );
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true; 
+		}
+		
+		//left
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true; 
+		}
+		
+		//Right
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true; 
+		}
+		
+		// below
+		p.setValues(position.getRow() + 1, position.getColumn() );
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true; 
 		}
 
-		p.setValues(position.getRow() - 2, position.getColumn() - 1);
-		if (getBoard().positionExists(p) && canMove(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
-		p.setValues(position.getRow() - 2, position.getColumn() + 1);
-		if (getBoard().positionExists(p) && canMove(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
-		p.setValues(position.getRow() - 1, position.getColumn() + 2);
-		if (getBoard().positionExists(p) && canMove(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
-		p.setValues(position.getRow() + 1, position.getColumn() + 2);
-		if (getBoard().positionExists(p) && canMove(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
-		p.setValues(position.getRow() + 2, position.getColumn() + 1);
-		if (getBoard().positionExists(p) && canMove(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
-		p.setValues(position.getRow() + 2, position.getColumn() - 1);
-		if (getBoard().positionExists(p) && canMove(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
-		p.setValues(position.getRow() + 1, position.getColumn() + 2);
-		if (getBoard().positionExists(p) && canMove(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
 		//nw
 		p.setValues(position.getRow() - 1, position.getColumn() -1 );
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
@@ -105,7 +100,6 @@ public class Knight extends ChessPiece {
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true; 
 		}
-		
 		
 		return mat;
 	}
